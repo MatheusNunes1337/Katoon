5d826b0e0c538da08038ef86b746221c5ac4072e
Matheus Nunes <matheus.silva@compasso.com.br>
2022-12-30 13:55:58 -0300

    feat: implement find method in the IUserRepository

--- src/app/repositories/IUserRepository.ts
+++ src/app/repositories/IUserRepository.ts
@@ -1,10 +1,11 @@
-import { IUser } from "../app/interfaces/IUser"
-import { IUserFilter } from "../app/interfaces/IUserFilter"
+import { IUser } from "../interfaces/IUser"
+import { IUserFilter } from "../interfaces/IUserFilter"
 import { ICreateUserDTO } from "../useCases/user/createUser/CreateUserDTO"
 
 export interface IUserRepository {
     save(user: ICreateUserDTO): Promise<void>
-    find(filter?: IUserFilter): Promise<Array<any>>
+    find(): Promise<Array<any>>
+    findByFilter(filter?: IUserFilter): Promise<Array<any>>
     findById(id: number): Promise<any>
     update(user: IUser, id: number): Promise<any>
     delete(id: number): Promise<void>
