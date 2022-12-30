2beddb21b3d141e6952f9943480130568729ec3a
Matheus Nunes <matheus.silva@compasso.com.br>
2022-12-30 14:43:07 -0300

    feat: add route to get a user by id

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import {Router, Request, Response} from 'express'
 import { createUserController } from '../app/useCases/user/createUser'
+import { findUserController } from '../app/useCases/user/findUser'
 import { findUsersController } from '../app/useCases/user/findUsers'
 
 const userRoute = Router()
@@ -10,6 +11,10 @@ userRoute.post('/', (request: Request, response: Response) => {
 
 userRoute.get('/', (request: Request, response: Response) => {
     return findUsersController.execute(request, response)
+})
+
+userRoute.get('/:id', (request: Request, response: Response) => {
+    return findUserController.execute(request, response)
 })
 
 export {userRoute}
