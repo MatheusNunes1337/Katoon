57ae83a6c26f8d0993d8ef32034af8a733e230c3
Matheus Nunes <matheus.silva@compasso.com.br>
2023-01-02 10:52:59 -0300

    feat: hash password in UpdateUserUseCase

--- src/app/useCases/user/updateUser/UpdateUserUseCase.ts
+++ src/app/useCases/user/updateUser/UpdateUserUseCase.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt'
 import { NotFoundError } from "../../../errors/NotFoundError";
 import { IUserRepository } from "../../../repositories/IUserRepository";
 import { IUpdateUserDTO } from "./UpdateUserDTO";
@@ -8,6 +9,12 @@ export class UpdateUserUseCase {
     async execute(payload: IUpdateUserDTO, id: number) {
         const userExists = await this.userRepository.findById(id)
         if(!userExists) throw new NotFoundError('User not found')
+
+        if('password' in payload) {
+            const {password} = payload
+            const salt = await bcrypt.genSalt(10)
+            payload.password = await bcrypt.hash(password!, salt)
+        }
 
         return await this.userRepository.update(payload, id)
     }
